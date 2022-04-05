c63ebb81a872c6dfe688cfb65f144cde491f98c6
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-04-05 08:54:37 +0200

    Create Triangle.cs

---
+++ Triangle.cs
@@ -0,0 +1,9 @@
+﻿namespace PhotorealisticRenderer.Shapes
+{
+    public class Triangle : Shape
+    {
+        public Triangle(Vector root, Vector eulerRotation) : base(root, eulerRotation)
+        {
+        }
+    }
+}
