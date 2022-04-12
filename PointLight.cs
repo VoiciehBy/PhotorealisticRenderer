3992bc21dc82b2b16f1e278b407b5be1929ab75a
VoiciehBy <43652981+VoiciehBy@users.noreply.github.com>
2022-04-12 11:48:27 +0200

    Added initial Directional and Point LLight class prototypes...

---
+++ PointLight.cs
@@ -0,0 +1,11 @@
+/*public PointLight : Light{
+    public Vector location;
+    int placeholder;
+    int placeholder1;
+    int placeholder2;
+
+    Vector getDiffuse(Vector cameraPosition,int placeholder);
+    Vector getSpecular(Vector cameraPosition,int placeholder);
+
+    bool isInShadow(int placeholder,Shape shape);
+}*/
